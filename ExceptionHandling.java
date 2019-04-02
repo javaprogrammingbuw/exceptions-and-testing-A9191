1ebf52bce99bd1c6a14f369ddf13764fb6016a15
Afshin Ghasemi <afshin.ghasemi@uni-weimar.de>
2019-04-02 13:35:39 +0200

    <final>

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,26 +1,17 @@
-package exceptions;
-
 public class ExceptionHandling {
 	
 	public static void main(String[] args) {		
-		//todo:
-		//1.) Execute the following program. It will throw Exceptions.
-		//Catch the Exceptions using their proper Names till no further
-		//exceptions occur.
-		//
-		//2.) Taking the square root of a negative number won't cause
-		//the exception. Write your own Exception for this case and 
-		//write a method which throws this exception			          
-		        int a = 5; 
-		        int b = 0; 		          
-		        try
-		        { 
-		            int i = a/b; 		          
-		        }  
-		        catch(ArithmeticException e) 
-		        { 
-		        	e.printStackTrace();; 
-		        } 
+			          
+		int a = 5; 
+		int b = 0; 		          
+		try
+		{ 
+			int i = a/b; 		          
+		}  
+		catch(ArithmeticException e) 
+		{ 
+			e.printStackTrace();; 
+		} 
        
 		int[] arr = new int[5];
 		try
@@ -60,6 +51,12 @@ public class ExceptionHandling {
 	    }
 	}
 }
-	
 
-
+//todo:
+//1.) Execute the following program. It will throw Exceptions.
+//Catch the Exceptions using their proper Names till no further
+//exceptions occur.
+//
+//2.) Taking the square root of a negative number won't cause
+//the exception. Write your own Exception for this case and 
+//write a method which throws this exception
